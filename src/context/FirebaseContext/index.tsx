ac6fb378f07d64e635d2995c964e10a8567db749
islam hafez <aboeshaeslam2@gmail.com>
2024-01-06 21:00:25 +0200

    enable google analytics

--- src/context/FirebaseContext/index.tsx
+++ src/context/FirebaseContext/index.tsx
@@ -1,6 +1,7 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { firebaseConfig } from "../../firebaseConfig";
 import { ReactNode, createContext } from "react";
+import { getAnalytics } from "firebase/analytics";
 
 export const FirebaseContext = createContext<FirebaseApp | undefined>(
   undefined
@@ -8,6 +9,7 @@ export const FirebaseContext = createContext<FirebaseApp | undefined>(
 
 export const FirebaseProvider = ({ children }: { children: ReactNode }) => {
   const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   return (
     <FirebaseContext.Provider value={app}>{children}</FirebaseContext.Provider>
   );
