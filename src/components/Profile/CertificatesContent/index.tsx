dd81852eff74dd87e007d312e92b587ea37f3cc3
islam hafez <aboeshaeslam2@gmail.com>
2024-02-01 11:53:09 +0200

    custom scroll-bar

--- src/components/Profile/CertificatesContent/index.tsx
+++ src/components/Profile/CertificatesContent/index.tsx
@@ -5,11 +5,11 @@ const CertificatesContent = () => {
   return (
     <ul className="flex flex-col gap-4">
       {certificateItemsDetails.map((item, index) => (
-        <li key={index}>
+        <li key={index} className="flex">
           <a
             href={item.link}
             target="_blank"
-            className="flex flex-wrap items-center gap-1 font-medium hover:text-gray-500 transition-all"
+            className="border-b-white flex flex-wrap items-center gap-1 font-medium hover:text-gray-500 transition-all"
           >
             {item.name},
             <span className="text-gray-500 text-sm italic font-normal">
--- src/components/Profile/MeContent/index.tsx
+++ src/components/Profile/MeContent/index.tsx
@@ -5,8 +5,8 @@ const MeContentTab = () => {
     <div>
       <p className="text-gray-300 font-medium">
         <img className="w-8 h-8 mb-3" src={hand} alt="hand" />
-        Welcome to My Portfolio! Enthusiastic Self-Taught Frontend Developer
-        with Dedication, Quick Learning, and Collaborative Spirit.
+        Welcome to My Portfolio! Self-Taught Frontend Developer with Dedication,
+        Quick Learning, and Collaborative Spirit.
       </p>
       <ul>
         <h3 className="my-4 text-2xl font-bold">What I Offer:</h3>
